5fbc1af59eee84b15a6812380e3c0cf443400ced
Anass-Chebbaki <S1099007@studenti.univpm.it>
2023-09-03 21:59:10 +0200

    uellaaaasssas

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MainApp());
 }
-//DIOSMEGMA dio bastardello
+//DIOSMEGMA dio bastardello aaaaasas
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
