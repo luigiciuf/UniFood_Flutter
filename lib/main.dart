1464e02de12c49a0ef86a71a08c6e5ed5d8f4f45
ciuffreda_luigi <92534853+luigiciuf@users.noreply.github.com>
2023-09-06 09:11:57 +0200

    barra di ricerca, categorie e prodotti update

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:unifood/firebase_options.dart';
-import 'package:unifood/Registrazione.dart'; // Importa la schermata di registrazione.
+import 'package:unifood/Registrazione.dart';
 import 'package:unifood/Login.dart';
 import 'package:unifood/models/Categorie.dart';
 import 'package:unifood/models/Prodotto.dart';
-import 'package:unifood/DatabaseManager.dart';// Importa la schermata di login.
+import 'package:unifood/DatabaseManager.dart';
 
 List<Categorie> categorie = [
   Categorie(nome: 'Pizza', imagePath: 'assets/images/cat_1.png'),
@@ -15,10 +15,10 @@ List<Categorie> categorie = [
   Categorie(nome: 'Insalate', imagePath: 'assets/images/cat_3.png'),
   Categorie(nome: 'Bibite', imagePath: 'assets/images/cat_4.png'),
   Categorie(nome: 'Dolci', imagePath: 'assets/images/cat_5.png'),
-
-  // Aggiungi altre categorie...
-
 ];
+
+List<Prodotto> listaProdotti = [];
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -41,7 +41,42 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchProdotti();
+  }
+
+  void fetchProdotti() async {
+    final List<Prodotto> prodotti = await DatabaseManager(context).getProdotti();
+    setState(() {
+      listaProdotti = prodotti;
+    });
+  }
+
+  void filterProdotti(String query) {
+    if (query.isEmpty) {
+      // Se la barra di ricerca è vuota, mostra tutti i prodotti
+      fetchProdotti();
+    } else {
+      List<Prodotto> filteredProdotti = listaProdotti
+          .where((prodotto) => prodotto.nome.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+
+      setState(() {
+        listaProdotti = filteredProdotti;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,9 +97,7 @@ class MyHomePage extends StatelessWidget {
                       color: Colors.red,
                     ),
                   ),
-
                   SizedBox(height: 8),
-
                   Text(
                     'Prenota subito il tuo pranzo!',
                     style: TextStyle(
@@ -72,93 +105,131 @@ class MyHomePage extends StatelessWidget {
                       fontStyle: FontStyle.italic,
                     ),
                   ),
-
                   SizedBox(height: 10),
-
-                  // Add your Image here using Image.asset('path_to_your_image.png'),
                   TextField(
+                    controller: searchController,
+                    onChanged: filterProdotti, // Assicurati che filterProdotti sia la funzione che filtra i prodotti
                     decoration: InputDecoration(
                       hintText: 'Cerca...',
                       prefixIcon: Icon(Icons.search),
-                      // Add your search_background image as a decoration
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.clear),
+                        onPressed: () {
+                          // Quando l'icona "X" viene premuta, cancella il testo nel campo di ricerca
+                          searchController.clear();
+                          // Chiamiamo la funzione di filtro per mostrare nuovamente tutti i prodotti
+                          filterProdotti('');
+                        },
+                      ),
                     ),
                   ),
-
-                  SizedBox(height: 40),
-
+                  SizedBox(height: 20),
                   Container(
-                    height: 120, // Imposta l'altezza desiderata per il ListView orizzontale
-                    child: ListView(
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      children: List.generate(categorie.length, (index) {
-                        return GestureDetector(
-                          onTap: () {
-                            // Azione quando si fa clic su una categoria
-                          },
-                          child: Container(
-                            margin: EdgeInsets.only(left: 5),
-                            decoration: BoxDecoration(
-                                color: Colors.orangeAccent,
-                                borderRadius: BorderRadius.circular(10)
-                            ),
-                            child: Column(
-                              children: [
-                                Image.asset(categorie[index].imagePath, width: 100, height: 100),
-                                SizedBox(height: 4),
-                                Text(
-                                    categorie[index].nome,
-                                  style: TextStyle(  // Aggiungi questo stile
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      }),
+                    width: double.infinity,
+                    height: 130,
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    decoration: BoxDecoration(
+                      color: Colors.orangeAccent,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Primo Del Giorno:',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
-                ],
-              ),
-            ),
-
-            SizedBox(height: 40),  // Aggiunge spazio tra le due liste
-
-            FutureBuilder<List<Prodotto>>(
-              future: DatabaseManager(context).getProdotti(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return CircularProgressIndicator();
-                } else if (snapshot.hasError) {
-                  return Text('Errore nel caricamento dei prodotti');
-                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return Text('Nessun prodotto disponibile');
-                } else {
-                  return Container(
-                    margin: EdgeInsets.only(left: 40),
-                    height: 100,
+                  SizedBox(height: 30),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 5),
+                        child: Text(
+                          'Categorie',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      Container(
+                        height: 130,
+                        child: ListView(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          children: List.generate(categorie.length, (index) {
+                            return GestureDetector(
+                              onTap: () {
+                                // Azione quando si fa clic su una categoria
+                              },
+                              child: Container(
+                                margin: EdgeInsets.only(left: 5),
+                                decoration: BoxDecoration(
+                                  color: Colors.orangeAccent,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Column(
+                                  children: [
+                                    Image.asset(categorie[index].imagePath, width: 100, height: 100),
+                                    Padding(
+                                      padding: EdgeInsets.symmetric(vertical: 0),
+                                      child: Text(
+                                        categorie[index].nome,
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          }),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 40),
+                  Padding(
+                    padding: EdgeInsets.only(left: 5),
+                    child: Text(
+                      'Prodotti',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Container(
+                    margin: EdgeInsets.only(left: 10),
+                    height: 200,
                     child: ListView.builder(
                       scrollDirection: Axis.horizontal,
-                      itemCount: snapshot.data!.length,
+                      itemCount: listaProdotti.length,
                       itemBuilder: (context, index) {
                         return Padding(
                           padding: const EdgeInsets.all(8.0),
                           child: Column(
                             children: [
-                              Image.network(snapshot.data![index].imgUri, width: 100, height: 100),
+                              Image.network(listaProdotti[index].imgUri, width: 100, height: 100),
                               SizedBox(height: 4),
-                              Text(snapshot.data![index].nome),
+                              Text(listaProdotti[index].nome),
                               SizedBox(height: 4),
-                              Text('€${snapshot.data![index].prezzo}'),
+                              Text('€${listaProdotti[index].prezzo}'),
                             ],
                           ),
                         );
                       },
                     ),
-                  );
-                }
-              },
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -172,7 +243,7 @@ class MyHomePage extends StatelessWidget {
               icon: Icon(Icons.checklist),
               onPressed: () {},
             ),
-            SizedBox(), // Placeholder for the center button
+            SizedBox(),
             IconButton(
               icon: Icon(Icons.settings),
               onPressed: () {},
